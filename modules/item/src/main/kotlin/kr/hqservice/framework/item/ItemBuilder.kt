00d71e3bfb9c21712c810241bffe2cbb3ab61f2b
BGMSound <86611784+BGMSound@users.noreply.github.com>
2023-05-02 03:00:23 +0900

    ItemBuilder
    ItemMeta Edit Exention

---
+++ modules/item/src/main/kotlin/kr/hqservice/framework/item/ItemBuilder.kt
@@ -0,0 +1,13 @@
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
+
+fun ItemStack.editMeta(block: ItemMeta.() -> Unit): ItemStack {
+    itemMeta = itemMeta?.apply(block::invoke)
+    return this
+}
+
+fun ItemMeta.addLine(line: String) {
+    lore = lore?.apply {
+        add(line)
+    }?: mutableListOf(line)
+}
