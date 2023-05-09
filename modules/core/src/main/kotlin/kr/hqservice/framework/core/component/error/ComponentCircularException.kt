16c5d2edcc74139d69f94c041362f047e543ff56
JunhyungPark <vjh0107@naver.com>
2023-05-09 09:10:37 +0900

    typo: fix

--- modules/core/src/main/kotlin/kr/hqservice/framework/core/component/error/ComponentCircularException.kt
+++ modules/core/src/main/kotlin/kr/hqservice/framework/core/component/error/ComponentCircularException.kt
@@ -3,7 +3,7 @@ package kr.hqservice.framework.core.component.error
 import kotlin.reflect.KClass
 
 class ComponentCircularException(val classes: List<KClass<*>>) : Exception(
-    "circular dependency found: " +
+    "circular dependencies found: " +
             classes.map { "\n$it" }.toString().replace("[", "").replace("]", "") + "\n" +
             ""
 )
