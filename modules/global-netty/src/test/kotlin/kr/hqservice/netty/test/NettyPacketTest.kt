417fbcd34c5712db70691e1c3cd0180527bb7dfc
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2023-05-18 23:32:42 +0900

    test: test netty in/outbound

---
+++ modules/global-netty/src/test/kotlin/kr/hqservice/netty/test/NettyPacketTest.kt
@@ -0,0 +1,64 @@
+package kr.hqservice.netty.test
+
+import be.seeseemelk.mockbukkit.MockBukkit
+import be.seeseemelk.mockbukkit.MockPlugin
+import be.seeseemelk.mockbukkit.ServerMock
+import io.netty.handler.codec.EncoderException
+import kr.hqservice.framework.netty.packet.Direction
+import kr.hqservice.framework.netty.packet.server.HandShakePacket
+import kr.hqservice.framework.netty.pipeline.ConnectionState
+import kr.hqservice.netty.test.global.TestBootstrap
+import kr.hqservice.yaml.extension.yaml
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.io.File
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+class NettyPacketTest {
+    private lateinit var server: ServerMock
+    private lateinit var plugin: MockPlugin
+
+    @BeforeEach
+    fun setup() {
+        server = MockBukkit.mock()
+        plugin = MockBukkit.createMockPlugin()
+    }
+
+    @Test
+    fun netty_inbound_test() {
+        val config = File("src/test/resources/config.yml").yaml()
+        val channel = TestBootstrap(plugin.logger, config).initTestChannel()
+
+        Direction.INBOUND.addListener(HandShakePacket::class) { packet, wrapper ->
+            wrapper.port = packet.port
+            wrapper.handler.setConnectionState(ConnectionState.CONNECTED)
+            println("received handshake packet ${packet.port}")
+            assertEquals(wrapper.port, 25545)
+        }
+
+        channel.writeInbound(HandShakePacket(25545))
+        channel.finish()
+    }
+
+    @Test
+    fun netty_outbound_test() {
+        val config = File("src/test/resources/config.yml").yaml()
+        val channel = TestBootstrap(plugin.logger, config).initTestChannel()
+
+        // unregistered packet -> throw
+        assertThrows<EncoderException> {
+            channel.writeOutbound(HandShakePacket(25545))
+        }
+
+        Direction.OUTBOUND.registerPacket(HandShakePacket::class)
+
+        channel.writeOutbound(HandShakePacket(25545))
+        channel.finish()
+    }
+
+    @AfterEach
+    fun teardown() {
+        MockBukkit.unmock()
+    }
+
+}
---
+++ modules/global-netty/src/test/kotlin/kr/hqservice/netty/test/global/TestBootstrap.kt
@@ -0,0 +1,27 @@
+package kr.hqservice.netty.test.global
+
+import io.netty.channel.embedded.EmbeddedChannel
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder
+import io.netty.handler.codec.LengthFieldPrepender
+import kr.hqservice.framework.netty.pipeline.BossHandler
+import kr.hqservice.framework.netty.pipeline.PacketDecoder
+import kr.hqservice.framework.netty.pipeline.PacketEncoder
+import kr.hqservice.yaml.config.HQYamlConfiguration
+import java.util.logging.Logger
+
+class TestBootstrap(
+    private val logger: Logger,
+    private val config: HQYamlConfiguration,
+) {
+    fun initTestChannel(): EmbeddedChannel {
+        val ch = EmbeddedChannel()
+        ch.pipeline()
+            .addLast("encode-filter", LengthFieldPrepender(8))
+            .addLast("decode-filter", LengthFieldBasedFrameDecoder(Integer.MAX_VALUE,0,8,0,8))
+            .addLast("packet-decoder", PacketDecoder())
+            .addLast("packet-encoder", PacketEncoder())
+            .addLast("handler-boss", BossHandler(ch, logger))
+        return ch
+    }
+
+}
