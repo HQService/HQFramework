32b5a833e7a2f67908bd14eaffd21d2bcc7576ce
JunhyungPark <vjh0107@naver.com>
2023-06-02 04:25:04 +0900

    fix: fix import mistakes

--- modules/proxy-bungee-core/src/main/kotlin/kr/hqservice/framework/bungee/HQFrameworkBungeePlugin.kt
+++ modules/proxy-bungee-core/src/main/kotlin/kr/hqservice/framework/bungee/HQFrameworkBungeePlugin.kt
@@ -2,9 +2,7 @@ package kr.hqservice.framework.bungee
 
 import kr.hqservice.framework.bungee.core.HQBungeePlugin
 import kr.hqservice.framework.global.core.HQPlugin
-import kr.hqservice.framework.proxy.core.api.event.EventManager
 import net.md_5.bungee.api.plugin.Plugin
-import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.core.qualifier.named
@@ -12,12 +10,6 @@ import org.koin.dsl.module
 import org.koin.ksp.generated.module
 
 abstract class HQFrameworkBungeePlugin : HQBungeePlugin() {
-    private val eventManager: EventManager by inject()
-
-    final override fun getEventManager(): EventManager {
-        return eventManager
-    }
-
     final override fun onPreLoad() {
         startKoin()
     }
