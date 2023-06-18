24b3be2fa8678e35629e04c1545a4e5188eba330
JunhyungPark <vjh0107@naver.com>
2023-06-19 08:01:52 +0900

    fix, feat: fix hasSpace nullable contents issue, add ItemStack#getDisplayName

--- modules/bukkit-inventory/src/main/kotlin/kr/hqservice/framework/inventory/util/InventoryExtension.kt
+++ modules/bukkit-inventory/src/main/kotlin/kr/hqservice/framework/inventory/util/InventoryExtension.kt
@@ -3,5 +3,5 @@ package kr.hqservice.framework.inventory.util
 import org.bukkit.inventory.Inventory
 
 fun Inventory.hasSpace(size: Int = 1): Boolean {
-    return this.storageContents.filter { it.type.isAir }.size >= size
+    return this.storageContents.filter { it?.type?.isAir != false }.size >= size
 }
