c76d29ab376cd0a70bf18f9f5518cdfbebf7dd97
CosineAA <rhdwlgns0725@gmail.com>
2023-06-28 15:13:04 +0900

    add: send vararg message

--- modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/extension/CommandSenderExtension.kt
+++ modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/extension/CommandSenderExtension.kt
@@ -5,3 +5,7 @@ import org.bukkit.command.CommandSender
 fun CommandSender.sendColorizedMessage(string: String) {
     this.sendMessage(string.colorize())
 }
+
+fun CommandSender.sendMessages(vararg messages: String?) {
+    messages.filterNotNull().forEach(::sendColorizedMessage)
+}
