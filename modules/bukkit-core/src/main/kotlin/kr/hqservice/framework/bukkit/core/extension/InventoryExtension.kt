2e49290e8ce68146397afeb2f07d11e2e1d99e21
CosineAA <rhdwlgns0725@gmail.com>
2023-06-28 15:13:31 +0900

    add: get empty slots, item addable checker

---
+++ modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/extension/InventoryExtension.kt
@@ -0,0 +1,17 @@
+package kr.hqservice.framework.bukkit.core.extension
+
+import org.bukkit.Material
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+
+fun Inventory.getEmptySlots(): Collection<Int> {
+    return storageContents.indices.filter { index ->
+        storageContents[index] == null || storageContents[index].type == Material.AIR
+    }
+}
+
+fun Inventory.isAddable(item: ItemStack): Boolean {
+    return storageContents.any {
+        it == null || it.type == Material.AIR || (it.isSimilar(item) && it.amount + item.amount <= it.maxStackSize)
+    }
+}
