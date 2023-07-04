c32355785acf6f6c51ac7487bc74840317cae7b1
JunhyungPark <vjh0107@naver.com>
2023-07-04 23:29:05 +0900

    feat: database featuring finished
    Co-authored-by: cccgh5 <47964442+cccgh5@users.noreply.github.com>

--- modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/DatabaseModule.kt
+++ modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/DatabaseModule.kt
@@ -3,15 +3,12 @@ package kr.hqservice.framework.database
 import kr.hqservice.framework.database.packet.PlayerDataSavedPacket
 import kr.hqservice.framework.global.core.component.Component
 import kr.hqservice.framework.global.core.component.HQModule
-import kr.hqservice.framework.global.core.extension.print
 import kr.hqservice.framework.netty.api.NettyServer
 
 @Component
 class DatabaseModule(private val nettyServer: NettyServer) : HQModule {
     override fun onEnable() {
-        nettyServer.registerInnerPacket(PlayerDataSavedPacket::class) { packet, _ ->
-            packet.print("packet received")
-        }
+        nettyServer.registerInnerPacket(PlayerDataSavedPacket::class) { packet, _ -> }
         nettyServer.registerOuterPacket(PlayerDataSavedPacket::class)
     }
 }
