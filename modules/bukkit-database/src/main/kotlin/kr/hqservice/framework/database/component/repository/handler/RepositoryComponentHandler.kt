f13bc94c3c2a17ee010175e1ad7113939530c3dd
JunhyungPark <vjh0107@naver.com>
2023-07-05 11:18:07 +0900

    fix: add mariadb compatibility when initializing tables

--- modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/component/repository/handler/RepositoryComponentHandler.kt
+++ modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/component/repository/handler/RepositoryComponentHandler.kt
@@ -2,21 +2,25 @@ package kr.hqservice.framework.database.component.repository.handler
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import kr.hqservice.framework.global.core.component.handler.ComponentHandler
-import kr.hqservice.framework.global.core.component.handler.HQComponentHandler
 import kr.hqservice.framework.coroutine.component.handler.CoroutineScopeComponentHandler
+import kr.hqservice.framework.database.component.datasource.handler.DataSourceComponentHandler
 import kr.hqservice.framework.database.component.repository.HQRepository
 import kr.hqservice.framework.database.component.repository.Table
-import kr.hqservice.framework.database.component.datasource.handler.DataSourceComponentHandler
+import kr.hqservice.framework.global.core.component.handler.ComponentHandler
+import kr.hqservice.framework.global.core.component.handler.HQComponentHandler
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.exists
 import org.koin.core.annotation.Named
 import org.koin.core.component.KoinComponent
+import java.sql.SQLSyntaxErrorException
+import java.util.logging.Logger
 import kotlin.reflect.full.findAnnotation
 
 @ComponentHandler(depends = [DataSourceComponentHandler::class, CoroutineScopeComponentHandler::class])
 class RepositoryComponentHandler(
-    @Named("main") private val mainCoroutineScope: CoroutineScope
+    @Named("main") private val mainCoroutineScope: CoroutineScope,
+    private val logger: Logger
 ) : HQComponentHandler<HQRepository>, KoinComponent {
     override fun setup(element: HQRepository) {
         val table = element::class.findAnnotation<Table>() ?: return
@@ -24,10 +28,16 @@ class RepositoryComponentHandler(
             val tableInstance = it.objectInstance ?: throw IllegalArgumentException("Table 의 object instance 를 가져올 수 없습니다.")
             mainCoroutineScope.launch {
                 element.getDataSource().query {
-                    if(tableInstance.exists()) {
-                        SchemaUtils.addMissingColumnsStatements(tableInstance, withLogs = false)
-                    } else {
-                        SchemaUtils.create(tableInstance)
+                    try {
+                        if(tableInstance.exists()) {
+                            SchemaUtils.addMissingColumnsStatements(tableInstance, withLogs = false)
+                        } else {
+                            SchemaUtils.create(tableInstance)
+                        }
+                    } catch (exception: ExposedSQLException) {
+                        if (exception.cause is SQLSyntaxErrorException) {
+                            logger.info("${tableInstance.tableName} 테이블을 생성하는 과정에서 mariadb 내부 문제로 인해 테이블 생성을 실패하였을 수 있습니다.")
+                        }
                     }
                 }
             }
