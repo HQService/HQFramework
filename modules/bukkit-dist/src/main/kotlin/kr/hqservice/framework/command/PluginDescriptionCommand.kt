0d4ef052bd8701841f759ae3ee5380eded78d9ae
JunhyungPark <vjh0107@naver.com>
2023-07-07 02:14:10 +0900

    remove: remove test codes

--- modules/bukkit-dist/src/main/kotlin/kr/hqservice/framework/command/PluginDescriptionCommand.kt
+++ modules/bukkit-dist/src/main/kotlin/kr/hqservice/framework/command/PluginDescriptionCommand.kt
@@ -2,14 +2,14 @@ package kr.hqservice.framework.command
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kr.hqservice.framework.bukkit.core.HQBukkitPlugin
-import kr.hqservice.framework.bukkit.core.extension.colorize
-import kr.hqservice.framework.command.component.*
+import kr.hqservice.framework.bukkit.core.extension.sendColorizedMessage
+import kr.hqservice.framework.command.component.CommandExecutor
+import kr.hqservice.framework.command.component.HQCommandNode
+import kr.hqservice.framework.command.component.ParentCommand
 import kr.hqservice.framework.global.core.component.Component
 import kr.hqservice.framework.global.core.extension.toHumanReadable
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
 import kotlin.coroutines.CoroutineContext
 
 @Component
@@ -25,7 +25,7 @@ class PluginDescriptionCommand : HQCommandNode, CoroutineScope {
         priority = 2
     )
     fun sendVersion(sender: CommandSender, plugin: HQBukkitPlugin) {
-        sender.sendMessage("&a버전: &7${plugin.description.version}".colorize())
+        sender.sendColorizedMessage("&a버전: &7${plugin.description.version}")
     }
 
     @CommandExecutor(
@@ -35,28 +35,6 @@ class PluginDescriptionCommand : HQCommandNode, CoroutineScope {
         priority = 3
     )
     fun sendAuthors(sender: CommandSender, plugin: HQBukkitPlugin) {
-        sender.sendMessage("&a개발자: &7${plugin.description.authors.toHumanReadable()}".colorize())
-    }
-
-    @CommandExecutor(
-        label = "testgreedy",
-        description = "",
-        isOp = true,
-        priority = 999
-    )
-    fun testGreedyArguments(sender: CommandSender, @ArgumentLabel("문자잉") string: String, @ContextKey("intint") int: Int, plugin: HQBukkitPlugin, stringNullable: String? = "default", intNullable: Int?) {
-        sender.sendMessage("$string, $int, ${plugin.name}, $stringNullable, $intNullable")
-    }
-
-    @CommandExecutor(
-        label = "testgreedy11",
-        description = "",
-        isOp = true,
-        priority = 999
-    )
-    suspend fun testGreedyArguments2(sender: Player, @ArgumentLabel("문자잉2") string: String, int: Int, plugin: HQBukkitPlugin) {
-        sender.sendMessage("$string, $int, ${plugin.name} 1111")
-        delay(3000)
-        sender.sendMessage("$string, $int, ${plugin.name}")
+        sender.sendColorizedMessage("&a개발자: &7${plugin.description.authors.toHumanReadable()}")
     }
 }
