6199f1082cbfc59a054918f0e5aaa11f80586aae
junhyung <park@junhyung.kr>
2023-07-22 03:43:01 +0900

    feat: add pessimistic lock Exposed entity extensions

--- modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/util/ExposedExtensions.kt
+++ modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/util/ExposedExtensions.kt
@@ -4,7 +4,19 @@ import kr.hqservice.framework.bukkit.core.extension.toByteArray
 import kr.hqservice.framework.bukkit.core.extension.toItemArray
 import kr.hqservice.framework.bukkit.core.extension.toItemStack
 import org.bukkit.inventory.ItemStack
+import org.jetbrains.exposed.dao.DaoEntityID
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
+import org.jetbrains.exposed.dao.entityCache
+import org.jetbrains.exposed.dao.exceptions.EntityNotFoundException
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.Op
+import org.jetbrains.exposed.sql.SizedIterable
+import org.jetbrains.exposed.sql.SqlExpressionBuilder
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.statements.api.ExposedBlob
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.vendors.ForUpdateOption
 
 fun ByteArray.toExposedBlob(): ExposedBlob {
     return ExposedBlob(this)
@@ -24,4 +36,37 @@ fun ExposedBlob.toItemStack(): ItemStack {
 
 fun ExposedBlob.toItemArray(): Array<ItemStack> {
     return this.bytes.toItemArray()
+}
+
+fun <ID : Comparable<ID>, E : Entity<ID>> EntityClass<ID, E>.findForUpdate(
+    forUpdateOption: ForUpdateOption = ForUpdateOption.ForUpdate,
+    op: SqlExpressionBuilder.() -> Op<Boolean>,
+): SizedIterable<E> {
+    TransactionManager.current().entityCache
+    val query = searchQuery(Op.build(op)).forUpdate(forUpdateOption)
+    return wrapRows(query)
+}
+
+fun <ID : Comparable<ID>, E : Entity<ID>> EntityClass<ID, E>.findByIdForUpdate(
+    id: EntityID<ID>
+): E? {
+    return this.testCache(id) ?: findForUpdate { this@findByIdForUpdate.table.id eq id }.firstOrNull()
+}
+
+fun <ID : Comparable<ID>, E : Entity<ID>> EntityClass<ID, E>.findByIdForUpdate(
+    id: ID
+): E? {
+    return findByIdForUpdate(DaoEntityID(id, this.table))
+}
+
+fun <ID : Comparable<ID>, E : Entity<ID>> EntityClass<ID, E>.getForUpdate(
+    id: EntityID<ID>
+): E {
+    return findByIdForUpdate(id) ?: throw EntityNotFoundException(id, this)
+}
+
+fun <ID : Comparable<ID>, E : Entity<ID>> EntityClass<ID, E>.getForUpdate(
+    id: ID
+): E {
+    return getForUpdate(DaoEntityID(id, this.table))
 }
