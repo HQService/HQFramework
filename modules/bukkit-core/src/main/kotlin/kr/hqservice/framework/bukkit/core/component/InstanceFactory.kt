9af67a1ca8da91a6434081b89a516d4b8ac6c788
junhyung <park@junhyung.kr>
2023-07-30 11:22:41 +0900

    remove: 잘못 생성되어 있는 클래스 삭제

--- modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/component/InstanceFactory.kt
+++
@@ -1,8 +0,0 @@
-package kr.hqservice.framework.bukkit.core.component
-
-import kr.hqservice.framework.bukkit.core.HQBukkitPlugin
-import org.koin.core.qualifier.Qualifier
-
-interface InstanceFactory<T> {
-    fun createInstance(plugin: HQBukkitPlugin, qualifier: Qualifier?, scopeQualifier: Qualifier?): T
-}
