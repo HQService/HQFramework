ca3f3d3fb36e85c580d5aa2d27e55a26d3999c3f
junhyung <park@junhyung.kr>
2023-08-23 07:00:57 +0900

    feat: HQYamlConfigurationSection 의 기본값을 설정할 수 있게 변경

--- modules/global-yaml/src/main/kotlin/kr/hqservice/framework/yaml/config/HQYamlConfigurationSection.kt
+++ modules/global-yaml/src/main/kotlin/kr/hqservice/framework/yaml/config/HQYamlConfigurationSection.kt
@@ -1,7 +1,9 @@
 package kr.hqservice.framework.yaml.config
 
 interface HQYamlConfigurationSection {
-    fun getString(key: String): String
+    fun getString(key: String, default: String = ""): String
+
+    fun findString(key: String): String?
 
     fun getKeys(): List<String>
 
@@ -9,13 +11,21 @@ interface HQYamlConfigurationSection {
 
     fun getIntegerList(key: String): List<Int>
 
-    fun getBoolean(key: String): Boolean
+    fun getBoolean(key: String, default: Boolean = false): Boolean
 
-    fun getInt(key: String): Int
+    fun findBoolean(key: String): Boolean?
 
-    fun getDouble(key: String): Double
+    fun getInt(key: String, default: Int = 0): Int
 
-    fun getLong(key: String): Long
+    fun findInt(key: String): Int?
+
+    fun getDouble(key: String, default: Double = 0.0): Double
+
+    fun findDouble(key: String): Double?
+
+    fun getLong(key: String, default: Long = 0L): Long
+
+    fun findLong(key: String): Long?
 
     fun set(key: String, value: Any?)
 
