ab8fd25a468c204b1d1c3b3baba7219e140cf5d8
junhyung <park@junhyung.kr>
2023-08-24 10:11:31 +0900

    fix: JVM Throwable 과 Kotlin Throwable 이 호환되지 않아서 생긴 문제 수정

--- modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/coroutine/component/exceptionhandler/handler/CoroutineScopeAdviceAnnotationHandler.kt
+++ modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/coroutine/component/exceptionhandler/handler/CoroutineScopeAdviceAnnotationHandler.kt
@@ -5,6 +5,8 @@ import kr.hqservice.framework.bukkit.core.coroutine.component.exceptionhandler.*
 import kr.hqservice.framework.bukkit.core.util.PluginScopeFinder
 import kr.hqservice.framework.global.core.component.handler.AnnotationHandler
 import kr.hqservice.framework.global.core.component.handler.HQAnnotationHandler
+import kr.hqservice.framework.global.core.extension.print
+import java.lang.Exception
 import kotlin.reflect.KFunction
 import kotlin.reflect.full.*
 import kotlin.reflect.jvm.jvmErasure
@@ -34,7 +36,9 @@ class CoroutineScopeAdviceAnnotationHandler : HQAnnotationHandler<CoroutineScope
                 get() = exceptionHandler.priority
 
             override fun handle(throwable: Throwable): HandleResult {
-                val exceptionClass = function.valueParameters.firstOrNull { it.type.jvmErasure.isSubclassOf(Exception::class) }
+                val exceptionClass = function.valueParameters.firstOrNull {
+                    it.type.jvmErasure.isSubclassOf(Exception::class) || it.type.jvmErasure.isSubclassOf(Throwable::class)
+                }
                     ?: throw IllegalStateException("ExceptionHandler 의 value parameter 에는 Exception 이 들어와야합니다. function name: ${function.name}, value parameters: ${function.valueParameters}")
                 if (throwable::class.starProjectedType == exceptionClass.type) {
                     function.call(obj, throwable)
