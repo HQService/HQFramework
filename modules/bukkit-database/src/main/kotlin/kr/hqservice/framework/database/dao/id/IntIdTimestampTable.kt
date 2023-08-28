1e67f2941f6895bfc9bfe427d716b7a97ef13a7b
junhyung <park@junhyung.kr>
2023-08-29 00:05:43 +0900

    feat: Timestamp Table 은 직접 타임스탬프를 오버라이딩 해야하게 변경

--- modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/dao/id/IntIdTimestampTable.kt
+++ modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/dao/id/IntIdTimestampTable.kt
@@ -2,12 +2,8 @@ package kr.hqservice.framework.database.dao.id
 
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.javatime.datetime
-import java.time.LocalDateTime
 
 abstract class IntIdTimestampTable(name: String, columnName: String = "id") : TimestampIdTable<Int>(name) {
     final override val id: Column<EntityID<Int>> = integer(columnName).autoIncrement().entityId()
     final override val primaryKey = PrimaryKey(id)
-    final override val createdAt = datetime("createdAt").clientDefault { LocalDateTime.now() }
-    final override val updatedAt = datetime("updatedAt").nullable()
 }
