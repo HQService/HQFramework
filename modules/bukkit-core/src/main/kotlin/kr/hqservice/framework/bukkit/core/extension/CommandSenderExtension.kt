426c293a24b2974f99539b2f1dc4dba80a6e8338
junhyung <park@junhyung.kr>
2023-09-01 21:21:43 +0900

    feat: NettyPlayer 가 색 메시지를 보낼 수 있는 함수 추가

--- modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/extension/CommandSenderExtension.kt
+++ modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/extension/CommandSenderExtension.kt
@@ -1,7 +1,13 @@
 package kr.hqservice.framework.bukkit.core.extension
 
+import kr.hqservice.framework.netty.api.NettyPlayer
+import kr.hqservice.framework.netty.extension.sendMessage
 import org.bukkit.command.CommandSender
 
 fun CommandSender.sendColorizedMessage(string: String) {
     this.sendMessage(string.colorize())
+}
+
+fun NettyPlayer.sendColorizedMessage(string: String, logging: Boolean = true) {
+    this.sendMessage(string.colorize(), logging)
 }
