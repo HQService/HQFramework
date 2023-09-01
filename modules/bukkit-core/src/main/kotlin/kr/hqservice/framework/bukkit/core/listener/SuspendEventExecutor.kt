ad5e3bc44c1fd360a4f1c89593a93e406a36b26f
junhyung <park@junhyung.kr>
2023-09-01 19:19:57 +0900

    fix, feat: 리스너에서 메인 디스패처를 중첩 디스패칭할 경우 데드락 해결 및 Concurrent 기능 추가

--- modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/listener/SuspendEventExecutor.kt
+++ modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/listener/SuspendEventExecutor.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kr.hqservice.framework.bukkit.core.HQBukkitPlugin
+import kr.hqservice.framework.bukkit.core.coroutine.extension.BukkitMain
 import org.bukkit.event.Event
 import org.bukkit.event.Listener
 import org.bukkit.plugin.EventExecutor
@@ -12,6 +13,7 @@ import java.lang.reflect.InvocationTargetException
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.full.callSuspend
+import kotlin.reflect.full.hasAnnotation
 
 /**
  * @param eventClass event handler 의 첫번째 인자
@@ -24,20 +26,35 @@ class SuspendEventExecutor(
 ) : EventExecutor {
     override fun execute(empty: Listener, event: Event) {
         if (eventClass.isInstance(event)) {
-            runBlocking {
-                plugin.launch(Dispatchers.Unconfined, CoroutineStart.UNDISPATCHED) {
-                    try {
-                        if (method.isSuspend) {
-                            method.callSuspend(listenerInstance, event)
-                        } else {
-                            method.call(listenerInstance, event)
-                        }
-                    } catch (exception: InvocationTargetException) {
-                        val cause = exception.cause ?: exception
-                        throw cause
-                    }
-                }.join()
+            val dispatcher = if (event.isAsynchronous) {
+                Dispatchers.Unconfined
+            } else {
+                Dispatchers.BukkitMain
             }
+            if (method.hasAnnotation<Concurrent>()) {
+                plugin.launch(dispatcher, CoroutineStart.UNDISPATCHED) {
+                    executeHandler(event)
+                }
+            } else {
+                runBlocking {
+                    plugin.launch(dispatcher, CoroutineStart.UNDISPATCHED) {
+                        executeHandler(event)
+                    }.join()
+                }
+            }
+        }
+    }
+
+    private suspend fun executeHandler(event: Event) {
+        try {
+            if (method.isSuspend) {
+                method.callSuspend(listenerInstance, event)
+            } else {
+                method.call(listenerInstance, event)
+            }
+        } catch (exception: InvocationTargetException) {
+            val cause = exception.cause ?: exception
+            throw cause
         }
     }
 }
