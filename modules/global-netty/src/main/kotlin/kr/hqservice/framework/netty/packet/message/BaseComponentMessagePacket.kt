e5adfee92d6c8f855084072a0e2b5bbb3c468f3d
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2023-09-02 17:17:18 +0900

    test: 임시

--- modules/global-netty/src/main/kotlin/kr/hqservice/framework/netty/packet/message/BaseComponentMessagePacket.kt
+++ modules/global-netty/src/main/kotlin/kr/hqservice/framework/netty/packet/message/BaseComponentMessagePacket.kt
@@ -1,5 +1,8 @@
 package kr.hqservice.framework.netty.packet.message
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import com.google.gson.JsonParser
 import io.netty.buffer.ByteBuf
 import kr.hqservice.framework.netty.api.NettyPlayer
 import kr.hqservice.framework.netty.packet.Packet
@@ -7,8 +10,46 @@ import kr.hqservice.framework.netty.packet.extension.readPlayers
 import kr.hqservice.framework.netty.packet.extension.readString
 import kr.hqservice.framework.netty.packet.extension.writePlayers
 import kr.hqservice.framework.netty.packet.extension.writeString
-import net.md_5.bungee.api.chat.BaseComponent
-import net.md_5.bungee.chat.ComponentSerializer
+import net.md_5.bungee.api.chat.*
+import net.md_5.bungee.api.chat.hover.content.*
+import net.md_5.bungee.chat.*
+
+private val parser = JsonParser()
+private val gson = GsonBuilder()
+    .registerTypeAdapter(
+        BaseComponent::class.java, ComponentSerializer()
+    ).registerTypeAdapter(
+        TextComponent::class.java, TextComponentSerializer()
+    ).registerTypeAdapter(
+        TranslatableComponent::class.java, TranslatableComponentSerializer()
+    ).registerTypeAdapter(
+        KeybindComponent::class.java, KeybindComponentSerializer()
+    ).registerTypeAdapter(
+        ScoreComponent::class.java, ScoreComponentSerializer()
+    ).registerTypeAdapter(
+        SelectorComponent::class.java, SelectorComponentSerializer()
+    ).registerTypeAdapter(
+        Entity::class.java, EntitySerializer()
+    ).registerTypeAdapter(
+        Text::class.java, TextSerializer()
+    ).registerTypeAdapter(
+        Item::class.java, ItemSerializer()
+    ).registerTypeAdapter(
+        ItemTag::class.java, ItemTag.Serializer()
+    ).setLenient().create()
+
+private fun BaseComponent.toCompress(): String {
+    return gson.toJson(this)
+}
+
+private fun String.toDecompress(): BaseComponent {
+    val jsonElement = parser.parse(this)
+    return if (jsonElement.isJsonArray) {
+        gson.fromJson(jsonElement, Array<BaseComponent>::class.java)
+    } else {
+        arrayOf(gson.fromJson(jsonElement, BaseComponent::class.java))
+    }.first()
+}
 
 class BaseComponentMessagePacket(
     var message: BaseComponent,
@@ -17,13 +58,16 @@ class BaseComponentMessagePacket(
 ) : Packet() {
 
     override fun write(buf: ByteBuf) {
-        buf.writeString(ComponentSerializer.toString(message))
+        // buf.writeString(ComponentSerializer.toString(message))
+        val compress = message.toCompress()
+        buf.writeString(compress)
+        println(compress)
         buf.writeBoolean(logging)
         buf.writePlayers(receivers)
     }
 
     override fun read(buf: ByteBuf) {
-        message = ComponentSerializer.parse(buf.readString()).first()
+        message = buf.readString().toDecompress()
         logging = buf.readBoolean()
         receivers = buf.readPlayers()
     }
