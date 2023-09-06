365245fd9b3a4968b7fa9c18fefafa8b7cf82cc1
junhyung <park@junhyung.kr>
2023-09-06 21:47:10 +0900

    feat: clearViewsAndClose 를 suspend 로 변경

--- modules/bukkit-inventory/src/main/kotlin/kr/hqservice/framework/view/navigator/impl/NavigatorImpl.kt
+++ modules/bukkit-inventory/src/main/kotlin/kr/hqservice/framework/view/navigator/impl/NavigatorImpl.kt
@@ -80,9 +80,11 @@ internal class NavigatorImpl : Navigator {
         currentView[player.uniqueId]?.clear()
     }
 
-    override fun clearViewsAndClose(player: Player) {
+    override suspend fun clearViewsAndClose(player: Player) {
         clearViews(player)
-        player.closeInventory()
+        withContext(Dispatchers.BukkitMain) {
+            player.closeInventory()
+        }
     }
 
     override fun current(playerId: UUID): View? {
