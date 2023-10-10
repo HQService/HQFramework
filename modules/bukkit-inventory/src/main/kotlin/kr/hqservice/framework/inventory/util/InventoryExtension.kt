0355aa1450588aca24977de3da660937ed7c3193
ichanghun <cccgh5@naver.com>
2023-10-10 23:39:58 +0900

    fix: 인벤토리 hasSpace 오류 수정

--- modules/bukkit-inventory/src/main/kotlin/kr/hqservice/framework/inventory/util/InventoryExtension.kt
+++ modules/bukkit-inventory/src/main/kotlin/kr/hqservice/framework/inventory/util/InventoryExtension.kt
@@ -1,31 +1,20 @@
 package kr.hqservice.framework.inventory.util
 
 import org.bukkit.Bukkit
-import org.bukkit.Material
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.PlayerInventory
-import java.util.*
 
 fun Inventory.hasSpace(size: Int = 1): Boolean {
     return this.storageContents.filter { it?.type?.isAir != false }.size >= size
 }
 
 fun PlayerInventory.hasSpace(vararg itemStack: ItemStack, repeat: Int = 1): Boolean {
-    val inventoryCloned = Bukkit.getServer().createInventory(null, 45).apply {
-        val uniqueItem = ItemStack(Material.STONE).apply {
-            this.amount = 64
-            val itemMeta = this.itemMeta?.apply { setDisplayName(UUID.randomUUID().toString()) }
-            this.itemMeta = itemMeta
-        }
-        repeat(8) {
-            addItem(uniqueItem)
-        }
-        if (this@hasSpace.extraContents.isNotEmpty()) {
-            addItem(*this@hasSpace.extraContents.filter { it != null && !it.type.isAir }.toTypedArray())
-        }
+    val inventoryCloned = Bukkit.getServer().createInventory(null, 36).apply {
         if (this@hasSpace.storageContents.isNotEmpty()) {
-            addItem(*this@hasSpace.storageContents.filter { it != null && !it.type.isAir }.toTypedArray())
+            this@hasSpace.storageContents.filter { it != null && !it.type.isAir }.forEachIndexed { index, itemStack ->
+                setItem(index, itemStack.clone())
+            }
         }
     }
     repeat(repeat) {
