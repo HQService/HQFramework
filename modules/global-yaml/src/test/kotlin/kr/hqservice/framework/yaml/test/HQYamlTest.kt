06010f9618060f5ca932eb798536324c7abde12d
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-05-22 13:22:27 +0900

    feat: HQYamlConfiguration에 float, contains, list 메서드 추가

--- modules/global-yaml/src/test/kotlin/kr/hqservice/framework/yaml/test/HQYamlTest.kt
+++ modules/global-yaml/src/test/kotlin/kr/hqservice/framework/yaml/test/HQYamlTest.kt
@@ -1,43 +1,64 @@
 package kr.hqservice.framework.yaml.test
 
+import kr.hqservice.framework.yaml.config.HQYamlConfiguration
 import kr.hqservice.framework.yaml.extension.yaml
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import java.io.File
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class HQYamlTest {
-
+    
+    private lateinit var file: File
+    private lateinit var config: HQYamlConfiguration
+    
+    @BeforeEach
+    fun setup() {
+        file = File("src/test/resources/config.yml")
+        config = file.yaml()
+    }
+    
     @Test
-    fun yamlTest() {
-        val file = File("src/test/resources/config.yml")
-        val yaml = file.yaml()
-        assertEquals(yaml.getString("host"), "")
-        assertEquals(yaml.getSection("netty")?.getString("host"), "127.0.0.1")
+    fun configTest() {
+        assertEquals(config.getString("host"), "")
+        assertEquals(config.getSection("netty")?.getString("host"), "127.0.0.1")
 
         val temp = File("src/test/resources/temp.yml")
         file.copyTo(temp, true)
 
         file.appendText("\ntest: hello")
-        assertEquals(yaml.getString("test"), "")
+        assertEquals(config.getString("test"), "")
 
-        yaml.reload()
-        yaml.getSection("netty")?.getKeys()?.forEach {
+        config.reload()
+        config.getSection("netty")?.getKeys()?.forEach {
             // it.print("netty-section-> ")
         }
-        assertEquals(yaml.getString("test"), "hello")
+        assertEquals(config.getString("test"), "hello")
 
         temp.copyTo(file, true)
         temp.delete()
     }
 
     @Test
-    fun test() {
-        val file = File("src/test/resources/config.yml")
-        val config = file.yaml()
-
-        val list = config.getLongList("long-list")
-        println(list)
+    fun get_test() {
+        val strings = config.getStringList("string-list")
+        val doubles = config.getDoubleList("double-list")
+        val floats = config.getFloatList("float-list")
+        val longs = config.getLongList("long-list")
+        val ints = config.getIntegerList("int-list")
+        println(strings)
+        println(doubles)
+        println(longs)
+        println(floats)
+        println(ints)
+    }
+    
+    @Test
+    fun contains_test() {
+        println("float contains: ${config.contains("float")}")
+        println("test contains: ${config.contains("contains.test")}")
+        println("test2 contains: ${config.contains("contains.test2")}")
     }
 }
