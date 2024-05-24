095103ca063414f469502c4e9149715a0998893d
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2024-05-25 00:58:02 +0900

    fix: player repository load 시점을 접속 후로 변경

--- modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/repository/player/packet/handler/PlayerConnectionPacketHandler.kt
+++ modules/bukkit-database/src/main/kotlin/kr/hqservice/framework/database/repository/player/packet/handler/PlayerConnectionPacketHandler.kt
@@ -1,8 +1,10 @@
 package kr.hqservice.framework.database.repository.player.packet.handler
 
 import kotlinx.coroutines.*
+import kr.hqservice.framework.bukkit.core.coroutine.bukkitDelay
 import kr.hqservice.framework.bukkit.core.coroutine.element.TeardownOptionCoroutineContextElement
 import kr.hqservice.framework.bukkit.core.coroutine.extension.BukkitMain
+import kr.hqservice.framework.bukkit.core.listener.HandleOrder
 import kr.hqservice.framework.bukkit.core.listener.Listener
 import kr.hqservice.framework.bukkit.core.listener.Subscribe
 import kr.hqservice.framework.bukkit.core.netty.event.AsyncNettyPacketReceivedEvent
@@ -19,13 +21,24 @@ import kr.hqservice.framework.netty.packet.player.PlayerConnectionState
 import kr.hqservice.framework.nms.event.PlayerDataPreLoadEvent
 import org.bukkit.Server
 import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerAttemptPickupItemEvent
+import org.bukkit.event.player.PlayerCommandPreprocessEvent
+import org.bukkit.event.player.PlayerCommandSendEvent
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.PluginManager
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import java.util.*
+import java.util.concurrent.ConcurrentLinkedDeque
 
 @Listener
 class PlayerConnectionPacketHandler(
+    private val plugin: Plugin,
     private val playerRepositoryRegistry: PlayerRepositoryRegistry,
     private val coroutineScope: CoroutineScope,
     @Qualifier("switch") private val switchDefermentLock: DefermentLock,
@@ -35,6 +48,38 @@ class PlayerConnectionPacketHandler(
     private val packetSender: PacketSender,
     private val nettyService: HQNettyService,
 ) {
+    private var lockedPlayer = ConcurrentLinkedDeque<UUID>()
+
+    @Subscribe(HandleOrder.FIRST)
+    fun pickup(event: PlayerAttemptPickupItemEvent) {
+        if (lockedPlayer.contains(event.player.uniqueId))
+            event.isCancelled = true
+    }
+
+    @Subscribe(HandleOrder.FIRST)
+    fun onClick(event: InventoryClickEvent) {
+        if (lockedPlayer.contains(event.whoClicked.uniqueId))
+            event.isCancelled = true
+    }
+
+    @Subscribe(HandleOrder.FIRST)
+    fun onClick(event: PlayerDropItemEvent) {
+        if (lockedPlayer.contains(event.player.uniqueId))
+            event.isCancelled = true
+    }
+
+    @Subscribe(HandleOrder.FIRST)
+    fun onClick(event: PlayerCommandPreprocessEvent) {
+        if (lockedPlayer.contains(event.player.uniqueId))
+            event.isCancelled = true
+    }
+
+    @Subscribe(HandleOrder.FIRST)
+    fun onClick(event: PlayerMoveEvent) {
+        if (lockedPlayer.contains(event.player.uniqueId))
+            event.isCancelled = true
+    }
+
     private suspend fun <T : Any> onLoad(player: Player, repository: PlayerRepository<T>) {
         val value = newSuspendedTransaction(coroutineScope.coroutineContext) {
             repository.load(player)
@@ -149,36 +194,54 @@ class PlayerConnectionPacketHandler(
     }
 
     @Subscribe
-    fun onLoad(event: PlayerDataPreLoadEvent): Unit = runBlocking blocking@{
-        var cancelled = false
-        event.player.isInvulnerable = true
-        if (nettyService.isEnable()) {
-            val lock = switchDefermentLock.findLock(event.player.uniqueId)
-            if (lock != null) {
-                switchDefermentLock.unlock(event.player.uniqueId)
-            } else {
-                switchDefermentLock.tryLock(event.player) whenTimedOut@{ player ->
-                    launch(Dispatchers.BukkitMain) {
-                        player.kickPlayer("데이터 저장 시점을 받아오지 못하였습니다.")
-                        cancelled = true
+    suspend fun onLoad(event: PlayerDataPreLoadEvent) {
+        coroutineScope {
+            var cancelled = false
+            lockedPlayer.add(event.player.uniqueId)
+            withContext(Dispatchers.BukkitMain) {
+                //event.player.addPotionEffect(PotionEffect(PotionEffectType.BLINDNESS, -1, 10), true)
+                server.onlinePlayers.forEach { it.hidePlayer(plugin, event.player) }
+            }
+
+            event.player.isInvulnerable = true
+            if (nettyService.isEnable()) {
+                val lock = switchDefermentLock.findLock(event.player.uniqueId)
+                if (lock != null) {
+                    switchDefermentLock.unlock(event.player.uniqueId)
+                } else {
+                    switchDefermentLock.tryLock(event.player) whenTimedOut@{ player ->
+                        launch(Dispatchers.BukkitMain) {
+                            player.kickPlayer("데이터 저장 시점을 받아오지 못하였습니다.")
+                            cancelled = true
+                        }.join()
                     }.join()
-                }.join()
+                }
             }
-        }
 
-        if (cancelled) {
+            if (cancelled) {
+                event.player.isInvulnerable = false
+                lockedPlayer.remove(event.player.uniqueId)
+                return@coroutineScope
+            }
+
+            playerRepositoryRegistry.getAll().forEach { repository ->
+                withContext(Dispatchers.IO) {
+                    onLoad(event.player, repository)
+                }
+            }
+            //loadJobs.joinAll()
+
+            withContext(Dispatchers.BukkitMain) {
+                server.onlinePlayers.forEach { it.showPlayer(plugin, event.player) }
+                //event.player.addPotionEffect(PotionEffect(PotionEffectType.BLINDNESS, 10, 10), true)
+            }
+
+            lockedPlayer.remove(event.player.uniqueId)
+            pluginManager.callEvent(PlayerRepositoryLoadedEvent(event.player))
+
+            bukkitDelay(30)
             event.player.isInvulnerable = false
-            return@blocking
         }
-
-        playerRepositoryRegistry.getAll().forEach { repository ->
-            withContext(Dispatchers.IO) {
-                onLoad(event.player, repository)
-            }
-        }
-        //loadJobs.joinAll()
-        event.player.isInvulnerable = false
-        pluginManager.callEvent(PlayerRepositoryLoadedEvent(event.player))
     }
 
     @Subscribe
