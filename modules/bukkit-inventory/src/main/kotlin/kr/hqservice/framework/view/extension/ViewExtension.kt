24aa3f1e8fd608d6a6fd399eaf07f3dbea9f2d8c
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2024-08-21 05:13:13 +0900

    fix -

--- modules/bukkit-inventory/src/main/kotlin/kr/hqservice/framework/view/extension/ViewExtension.kt
+++ modules/bukkit-inventory/src/main/kotlin/kr/hqservice/framework/view/extension/ViewExtension.kt
@@ -4,13 +4,14 @@ import kr.hqservice.framework.view.View
 import kr.hqservice.framework.view.ViewModel
 import kr.hqservice.framework.view.ViewModelFactory
 import org.koin.java.KoinJavaComponent.getKoin
+import kotlin.reflect.KClass
 
-//val _viewModelFactory: ViewModelFactory by getKoin().inject()
+val _viewModelFactory: ViewModelFactory by getKoin().inject()
 
-/*inline fun <reified T : ViewModel> View.viewModels(): Lazy<T> {
+fun <T : ViewModel> View.viewModels(modelClass: KClass<T>): Lazy<T> {
     return lazy {
-        _viewModelFactory.provideViewModel(T::class) as T
+        _viewModelFactory.provideViewModel(modelClass) as T
     }.apply {
         this@viewModels._childLifecycles.add(this.value)
     }
-}*/
+}
