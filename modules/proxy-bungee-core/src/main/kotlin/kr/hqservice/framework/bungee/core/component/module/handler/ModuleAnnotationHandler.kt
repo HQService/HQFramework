83874a61c84446bbbf1e6c30e5b42a11a4b8b667
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-10-21 17:12:56 +0900

    feat: Module, Setup, Teardown 어노테이션 추가

---
+++ modules/proxy-bungee-core/src/main/kotlin/kr/hqservice/framework/bungee/core/component/module/handler/ModuleAnnotationHandler.kt
@@ -0,0 +1,42 @@
+package kr.hqservice.framework.bungee.core.component.module.handler
+
+import kotlinx.coroutines.runBlocking
+import kr.hqservice.framework.bungee.core.component.module.Module
+import kr.hqservice.framework.bungee.core.component.module.Setup
+import kr.hqservice.framework.bungee.core.component.module.Teardown
+import kr.hqservice.framework.global.core.component.handler.AnnotationHandler
+import kr.hqservice.framework.global.core.component.handler.HQAnnotationHandler
+import kotlin.reflect.full.callSuspend
+import kotlin.reflect.full.declaredMemberFunctions
+import kotlin.reflect.full.hasAnnotation
+
+@AnnotationHandler
+class ModuleAnnotationHandler : HQAnnotationHandler<Module> {
+    override fun setup(instance: Any, annotation: Module) {
+        instance::class.declaredMemberFunctions
+            .filter { it.hasAnnotation<Setup>() }
+            .forEach {
+                if (it.isSuspend) {
+                    runBlocking {
+                        it.callSuspend(instance)
+                    }
+                } else {
+                    it.call(instance)
+                }
+            }
+    }
+
+    override fun teardown(instance: Any, annotation: Module) {
+        instance::class.declaredMemberFunctions
+            .filter { it.hasAnnotation<Teardown>() }
+            .forEach {
+                if (it.isSuspend) {
+                    runBlocking {
+                        it.callSuspend(instance)
+                    }
+                } else {
+                    it.call(instance)
+                }
+            }
+    }
+}
---
+++ modules/proxy-velocity-core/src/main/kotlin/kr/hqservice/framework/velocity/core/component/module/Teardown.kt
@@ -0,0 +1,5 @@
+package kr.hqservice.framework.velocity.core.component.module
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Teardown
