1205d7c36394f50c511ae6ee34c2868a8a6fc13e
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-10-21 17:12:10 +0900

    feat: Module 어노테이션을 사용하도록 변경

--- modules/proxy-velocity-core/src/main/kotlin/kr/hqservice/framework/velocity/core/netty/NettyModule.kt
+++ modules/proxy-velocity-core/src/main/kotlin/kr/hqservice/framework/velocity/core/netty/NettyModule.kt
@@ -1,32 +1,28 @@
 package kr.hqservice.framework.velocity.core.netty
 
 import com.google.gson.Gson
-import com.velocitypowered.api.event.PostOrder
-import com.velocitypowered.api.event.Subscribe
-import com.velocitypowered.api.event.proxy.ProxyShutdownEvent
-import kr.hqservice.framework.velocity.core.netty.registry.NettyChannelRegistry
-import kr.hqservice.framework.global.core.component.Component
-import kr.hqservice.framework.global.core.component.HQModule
 import kr.hqservice.framework.velocity.core.HQVelocityPlugin
+import kr.hqservice.framework.velocity.core.component.module.Module
+import kr.hqservice.framework.velocity.core.component.module.Setup
+import kr.hqservice.framework.velocity.core.component.module.Teardown
 import kr.hqservice.framework.velocity.core.netty.listener.PlayerConnectionListener
 import kr.hqservice.framework.velocity.core.netty.listener.PlayerLastConnectionListener
+import kr.hqservice.framework.velocity.core.netty.registry.NettyChannelRegistry
 import kr.hqservice.framework.yaml.config.HQYamlConfiguration
 import java.io.File
 
-@Component
+@Module
 class NettyModule(
     config: HQYamlConfiguration,
     private val plugin: HQVelocityPlugin,
     private val bootstrap: NettyServerBootstrap,
     private val channelContainer: NettyChannelRegistry
-) : HQModule {
-    companion object {
-        private var state = false
-    }
+) {
     private val nettyEnabled: Boolean = config.getBoolean("netty.enabled")
     private val listener = PlayerLastConnectionListener(plugin)
 
-    override fun onEnable() {
+    @Setup
+    fun setup() {
         if (state) return
         state = true
 
@@ -38,12 +34,17 @@ class NettyModule(
         }
     }
 
-    override fun onDisable() {
+    @Teardown
+    fun teardown() {
         val file = File("last-connection.json")
         if (!file.exists()) file.createNewFile()
 
         file.writeText(Gson().toJson(listener.data))
 
         bootstrap.shutdown()
+    }
+
+    companion object {
+        private var state = false
     }
 }
