ba01904bb4a277ecf783c791579c450df787576e
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2025-01-18 21:49:31 +0900

    feat: HQYamlConfiguration에서 colorize한 String, List<String> 가져오는 함수 추가

--- modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/extension/HQYamlConfigurationExtension.kt
+++ modules/bukkit-core/src/main/kotlin/kr/hqservice/framework/bukkit/core/extension/HQYamlConfigurationExtension.kt
@@ -3,11 +3,23 @@ package kr.hqservice.framework.bukkit.core.extension
 import kr.hqservice.framework.yaml.config.HQYamlConfigurationSection
 import org.bukkit.Material
 
+fun HQYamlConfigurationSection.findMaterial(path: String): Material? {
+    return getString(path).uppercase().run(Material::getMaterial)
+}
+
 fun HQYamlConfigurationSection.getMaterial(path: String): Material {
     val materialText = getString(path).uppercase()
     return Material.getMaterial(materialText) ?: throw NullPointerException("${materialText}은(는) 존재하지 않는 Material입니다.")
 }
 
-fun HQYamlConfigurationSection.findMaterial(path: String): Material? {
-    return getString(path).uppercase().run(Material::getMaterial)
+fun HQYamlConfigurationSection.findColorizedString(path: String): String? {
+    return findString(path)?.colorize()
+}
+
+fun HQYamlConfigurationSection.getColorizedString(path: String): String {
+    return getString(path).colorize()
+}
+
+fun HQYamlConfigurationSection.getColorizedStringList(path: String): List<String> {
+    return getStringList(path).map(String::colorize)
 }
