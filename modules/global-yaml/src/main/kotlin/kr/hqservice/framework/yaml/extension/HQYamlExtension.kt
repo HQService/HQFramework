d0194932179cc758c3935df587446b71c26a4e3b
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2025-01-18 21:44:35 +0900

    feat: HQYamlConfiguration에서 UUID를 가져올 수 있는 함수 추가

--- modules/global-yaml/src/main/kotlin/kr/hqservice/framework/yaml/extension/HQYamlExtension.kt
+++ modules/global-yaml/src/main/kotlin/kr/hqservice/framework/yaml/extension/HQYamlExtension.kt
@@ -3,6 +3,22 @@ package kr.hqservice.framework.yaml.extension
 import kr.hqservice.framework.yaml.config.HQYamlConfiguration
 import kr.hqservice.framework.yaml.config.impl.HQYamlConfigurationImpl
 import java.io.File
+import java.util.UUID
 
 fun File.yaml(): HQYamlConfiguration =
     HQYamlConfigurationImpl().also { it.load(this) }
+
+fun HQYamlConfiguration.findUUID(path: String): UUID? {
+    return runCatching { getString(path).run(UUID::fromString) }.getOrNull()
+}
+
+fun HQYamlConfiguration.getUUID(path: String): UUID? {
+    val uniqueIdText = getString(path)
+    return runCatching {
+        UUID.fromString(uniqueIdText)
+    }.getOrNull() ?: throw NullPointerException("${uniqueIdText}은(는) 존재하지 않는 UUID입니다.")
+}
+
+fun HQYamlConfiguration.getUUIDList(path: String): List<UUID> {
+    return getStringList(path).map(UUID::fromString)
+}
